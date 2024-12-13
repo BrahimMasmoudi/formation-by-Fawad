8e3d4c0714cb4a9267118d04292527da3980d571
BrahimMasmoudi <masmoudi.brahim@gmail.com>
2024-12-13 16:26:26 +0100

    done

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+  {{error}}
+  <NuxtLink to="/">Go Home</NuxtLink>
+</template>
+<script setup lang="ts">
+import type {NuxtError} from '#app'
+defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<i18n>
+fr:
+  error: "Une erreur est survenue"
+  go-back: 'Retour à la page initiale'
+en:
+  create: An error occurred
+  go-back: 'Go back home'
+</i18n>
